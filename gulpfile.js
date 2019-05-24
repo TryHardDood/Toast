a76339e47d2ff704b4dd10cf2d7035cd9405273d
TryHardDood <rsnconfigs@gmail.com>
2019-05-24 12:50:23 +0200

    update(toast): Make changes to project structure
    - Switch from css to scss
    - Support es6

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
-const { src, dest, parallel } = require('gulp');
-const concat = require('gulp-concat');
+const {src, dest, parallel} = require('gulp');
+const concat = require('gulp-concat')
+;
 const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
 
 function css() {
-  return src('app/css/*.css')
-    .pipe(concat('toast.min.css'))
-    .pipe(cleanCSS())
-    .pipe(dest('build/css'))
+    return src('app/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('toast.min.css'))
+        .pipe(cleanCSS())
+        .pipe(dest('build/css'))
 }
 
+const uglifyes = require('uglify-es');
+const composer = require('gulp-uglify/composer');
+const uglify = composer(uglifyes, console);
+
 function js() {
-  return src('app/js/*.js', { sourcemaps: true })
-    .pipe(uglify())
-	.pipe(concat('toast.min.js'))
-    .pipe(dest('build/js', { sourcemaps: true }))
+    return src('app/js/*.js', {sourcemaps: true})
+        .pipe(uglify())
+        .pipe(concat('toast.min.js'))
+        .pipe(dest('build/js', {sourcemaps: true}))
 }
 
 exports.js = js;
